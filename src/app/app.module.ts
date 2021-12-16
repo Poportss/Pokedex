5d6583a3b5752c4e99f75fbe85c33b016d8cce76
Poportss <81190957+Poportss@users.noreply.github.com>
2021-12-16 12:46:15 -0300

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//modules
+import { PagesModule } from './pages/pages.module';
+
+//Components
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
